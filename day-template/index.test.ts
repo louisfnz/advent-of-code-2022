e3931d3f3910f3de411805425365782f17d5e803
louisfnz <25600218+louisfnz@users.noreply.github.com>
2022-12-01 19:25:34 +1300

    Update day template

--- day-template/index.test.ts
+++ day-template/index.test.ts
@@ -1,6 +1,5 @@
 import {part2, part1} from './index';
 
-// Stick the example in the problem description here:
 const exampleInputOne = `
 Example input value 1
 Example input value 2
@@ -9,18 +8,17 @@ Example input value 4
 Example input value 5
 `.trim();
 
-// ... and the example answers from the problem description here:
 const expectedAnswerPart1 = 15;
 const expectedAnswerPart2 = 120;
 
 describe('Day 1', () => {
   it('Calculates part 1 correctly', () => {
-    const result1 = part1(exampleInputOne);
-    expect(result1).toEqual(expectedAnswerPart1);
+    const result = part1(exampleInputOne);
+    expect(result).toEqual(expectedAnswerPart1);
   });
 
   it('Calculates part 2 correctly', () => {
-    const result1 = part2(exampleInputOne);
-    expect(result1).toEqual(expectedAnswerPart2);
+    const result = part2(exampleInputOne);
+    expect(result).toEqual(expectedAnswerPart2);
   });
 });
